1700a1b80e718729afd640d1a6857b1e042982f2
hbxks18 <719150800@qq.com>
2020-05-12 13:20:25 +0800

    config🔧 更新proxy配置方法

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,15 @@
-const proxy = require("http-proxy-middleware")
+const { createProxyMiddleware } = require("http-proxy-middleware")
 
 const proxyConf = {
   "/api": "http://localhost:3000",
 }
 
 module.exports = app => {
-  const proxys = Object.keys(proxyConf).forEach(uri => {
-    proxy(uri, { target: proxyConf[uri], changeOrigin: true })
+  const proxys = Object.keys(proxyConf).map(uri => {
+    return createProxyMiddleware(uri, {
+      target: proxyConf[uri],
+      changeOrigin: true,
+    })
   })
   app.use.apply(app, proxys)
 }
