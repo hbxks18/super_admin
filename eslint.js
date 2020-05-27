9471bab0b9be172104373cff8ccf68d7630483ff
hbxks18 <719150800@qq.com>
2020-05-27 15:58:59 +0800

    feat✨ eslint

---
+++ eslint.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["airbnb", "sf-js", "sf-react"],
+  rules: {
+    // Allow unused vars when you are debuging, but can't build.
+    "no-unused-vars": process.env.NODE_ENV === "development" ? "warn" : "error",
+  },
+}
