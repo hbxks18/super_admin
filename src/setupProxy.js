7cbe1f123e5a1d57a90bd66c279bfbb03de786e5
hbxks18 <719150800@qq.com>
2020-06-02 14:42:54 +0800

    format🥚

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,16 @@
 const { createProxyMiddleware } = require("http-proxy-middleware")
-const easymock = ""
+const easymock = "http://easymock.com" // 此处可以将接口代理到easy mock 地址，或者后端服务器地址
 
+// 根据需要修改代理的接口的前缀
 const proxyConf = {
-  "/api": easymock,
+  "/api": easymock
 }
 
 module.exports = app => {
   const proxys = Object.keys(proxyConf).map(uri => {
     return createProxyMiddleware(uri, {
       target: proxyConf[uri],
-      changeOrigin: true,
+      changeOrigin: true
     })
   })
   app.use.apply(app, proxys)
